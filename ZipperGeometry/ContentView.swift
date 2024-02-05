6434cfe441b1050687915acd36e08ba483ad04f1
Zoe Cutler <36316793+zcutlerf@users.noreply.github.com>
2024-02-05 16:18:49 -0500

    Initial Commit

---
+++ ZipperGeometry/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZipperGeometry
+//
+//  Created by Zoe Cutler on 2/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZipperGeometry/ZipperGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZipperGeometryApp.swift
+//  ZipperGeometry
+//
+//  Created by Zoe Cutler on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ZipperGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
